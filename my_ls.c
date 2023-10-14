d7e5a7937cffc1dd694f9d7cf3471a2e06a42c9a
mathius johnson <johnson_m@qwasar.io>
2023-10-14 19:22:06 +0000

    feat: pass all tests

--- my_ls.c
+++ my_ls.c
@@ -28,12 +28,16 @@ typedef struct s_dirnode {
 void read_files(filenode *list, int sort_time) {
   while (list != NULL) {
     if (strlen(list->val) > 0) {
-      printf("%s\n", list->val);
-      // if (sort_time) {
+    //   printf("%s\n", list->val);
+    //   if (sort_time) {
+    //   printf("%s\n", list->val);
 
-      //   printf("%lu\n", list->st_mtim.tv_nsec);
-      //   printf("%lu\n", list->st_mtim.tv_sec);
-      // }
+    //     printf("%lu\n", list->st_mtim.tv_nsec);
+    //     printf("%lu\n", list->st_mtim.tv_sec);
+    //   } else {
+                printf("%s\n", list->val);
+
+    //   }
     }
     list = list->next;
   }
@@ -118,6 +122,7 @@ int selection_sort_time(filenode *list) {
           // if nanoseconds are greater
           (min_time.tv_sec == iter_time.tv_sec &&
            min_time.tv_nsec < iter_time.tv_nsec)) {
+            //    printf("nanoseconds greater\n");
         min_node = iter;
         min_time = iter_time;
         was_altered = 1;
@@ -125,10 +130,12 @@ int selection_sort_time(filenode *list) {
           // if both are equal and strings are not equal
           (min_time.tv_nsec == iter_time.tv_sec &&
            min_time.tv_nsec == iter_time.tv_nsec) {
-        printf("times are equal\n");
+        // printf("times are equal\n");
         min_node = iter;
         min_time = iter_time;
         was_altered = 1;
+      } else {
+        //   printf("doesn't check out\n");
       }
       // If time is equal, and
     }
@@ -184,23 +191,38 @@ int is_batch_created(filenode *list) {
   return is_batch_created;
 }
 
-void *reverse_linked_list(filenode *head) {
+filenode *reverse_linked_list(filenode *head) {
   filenode *temp = head;
   filenode *prev = NULL;
   filenode *next = NULL;
+  filenode *first_dotfile = NULL;
+  int show_dotfiles = 0;
+    if (strcmp(temp->val, ".") == 0) {
+        show_dotfiles = 1;
+            //   prev = temp;
+        first_dotfile = temp;
+        // temp = temp->next;
+        next = temp->next;
+    //   prev = temp;
+      temp = next;
+    }
+  while (temp->next != NULL) {
+ if (strcmp(temp->val, ".") != 0) {
 
-  while (temp != NULL) {
-      printf("iterating\n");
-    if (strcmp(prev->val, ".") != 0 && strcmp(prev->val, "..") &&
-        strcmp(next->val, ".") && strcmp(next->val, "..")) {
       next = temp->next;
       temp->next = prev;
       prev = temp;
       temp = next;
-    }
+ }    //   printf("checks out %s\n", temp->val);
   }
-//   head = prev;
-//   return head;
+  if (show_dotfiles) {
+    //   printf("past while\n");
+    first_dotfile->next = prev;
+    head = first_dotfile;
+  } else {
+    head = prev;
+  }
+  return head;
 }
 
 void *check_list(dirnode *curr_dir, int show_hidden, int sort_time) {
@@ -221,10 +243,11 @@ void *check_list(dirnode *curr_dir, int show_hidden, int sort_time) {
   if (sort_time) {
     filenode *altered;
     altered = head_file;
-    int was_altered = selection_sort_time(altered);
+    selection_sort_time(altered);
     if (is_batch_created(altered)) {
-      printf("is batch created!!!!\n");
-      head_file = sort_files(altered);
+    //   printf("is batch created!!!!\n");
+    //   head_file = sort_files(altered);
+      head_file = reverse_linked_list(head_file);
     }
   } else {
     sort_files(head_file);
